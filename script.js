28948ae24b497e06424b142b2a4e5e738097e8ab
PINPAL <josh.hayes614@gmail.com>
2019-05-23 23:57:52 +0100

    Disable Scrolling with Popup

--- script.js
+++ script.js
@@ -152,6 +152,7 @@ const modifyPopup = document.getElementById("modifyPopup")
 const popupButtonWrapper = document.getElementsByClassName('popupButtonWrapper')[0]
 // Modify Button
 function modifyButton() {
+    disableScroll()
     modifyPopup.style.visibility = "visible"
     modifyPopup.style.backgroundColor = "rgba(0,0,0,0.6)"
     popupButtonWrapper.style.transform = "translate(-50%, 0%)"
@@ -167,5 +168,39 @@ modifyPopup.addEventListener("click", function (e) {
 function hidePopup() {
     popupButtonWrapper.style.transform = "translate(-50%, calc(100% + 10px))"
     modifyPopup.style.backgroundColor = "rgba(0,0,0,0)"
+    enableScroll()
     setTimeout(function(){modifyPopup.style.visibility = "hidden"}, 350);
+}
+
+//Functions for enabling and disabling scrolling
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  document.addEventListener('wheel', preventDefault, {passive: false}); // Disable scrolling in Chrome
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    document.removeEventListener('wheel', preventDefault, {passive: false}); // Enable scrolling in Chrome
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
 }
