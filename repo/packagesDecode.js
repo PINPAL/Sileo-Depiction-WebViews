577fdc88f7afd2f50e9008509b90cbd6a6aca512
PINPAL <josh.hayes614@gmail.com>
2019-07-03 16:38:26 +0100

    Refactor + Major Code Cleanup
    • Breaks some stuff too dw

---
+++ repo/packagesDecode.js
@@ -0,0 +1,65 @@
+// Function to return all different categories and amount of tweaks in them
+function returnCategoryCount(categoriesDump) {
+    // Define Variables
+    let categories = []
+    let count = []
+    let contains = false
+    // Loop through all categories dump (duplicates in one giant array)
+    for (i=0; i<categoriesDump.length; i++) {
+        var tempCategoryName = categoriesDump[i].replace(/Section:\s/g,"")
+        contains = false
+        // Loop through existing categories
+        for (j=0; j<categories.length; j++) {
+            // If already in existing category, increment the counter
+            if (categories[j] == tempCategoryName) {
+                contains = true
+                count[j]++
+            }
+        }
+        // If not in existing category, add it to it.
+        if (!contains) {
+            categories.push(tempCategoryName)
+            count.push(1)
+        }
+    }
+    // Return into 2 dimensional array
+    return [categories, count]
+}
+
+// Function to decode Packages File
+function decodePackagesFile(packagesFile) {
+    // Clean empty lines out of packages File
+    packagesFile = packagesFile.replace(/^\s*[\r\n]/gm,"")
+    // Split PackagesFile into Packages
+    let packages = packagesFile.split("Package:")
+    // Remove first blank from Packages
+    packages.shift()
+    console.log(packages)
+    // Loop through packages and convert them to objects
+    for (packageNum = 0; packageNum < packages.length; packageNum++) {
+        // Define the package
+        let package = {}
+        // Set the package's bundle ID property
+        Object.defineProperty(package, "Package", {
+            value: packages[packageNum].split('\n', 1)[0].replace(" ","")
+        })
+        // Remove Bundle ID line from Package String
+        packages[packageNum] = packages[packageNum].slice(packages[packageNum].indexOf('\n') + 1)
+        // Get all remaining properties of package
+        let properties = packages[packageNum].split("\n")
+        for (i=0; i<properties.length; i++) {
+            // Extract Property Name from before first colon
+            let propertyName = properties[i].substring(0, properties[i].indexOf(":"))
+            // Extract Property Value from after first colon
+            let propertyValue = properties[i].substring(properties[i].indexOf(":") + 2, properties[i].length)
+            // Set the property within the object
+            Object.defineProperty(package, propertyName, {
+                value: propertyValue
+            })
+        }
+        // Convert the package from string into new object
+        packages[packageNum] = package
+    }
+    console.log(packages)
+    return packages
+}
--- repo/script.js
+++ repo/script.js
@@ -1,15 +1,31 @@
-// Define featuredBannersView
-const featuredBannersView = document.getElementById("FeaturedBannersView")
 
 // Get Repo from URL
-const currentDirectory = window.location.origin + window.location.pathname.replace("index.html","")
 var repoURL = getQueryVariable("repo")
+// Append slash to end of repo URL (if required)
+if (repoURL.slice(-1) != "/") {
+    repoURL += "/"
+}
+
+// Fetch Repo Packages File
+var validPackagesFile = true
+try {
+    var packagesFile = loadFile(repoURL + "Packages")
+} catch (error) {
+    validPackagesFile = false
+}   
 
 // Fetch Repo Title from Cydia's Release File
+var repoTitle = "Repo Title"
 try {
-    var repoTitle = loadFile(repoURL + "/Release").match(/Label:.*/)[0].replace(/Label:\s/g,"")
+    var repoTitle = loadFile(repoURL + "Release").match(/Label:.*/)[0].replace(/Label:\s/g,"")
+} catch (error) {}
+
+// Fetch Sileo Featured JSON from Repo
+var validSileoFeatured = true
+try {
+    var sileoFeaturedJSON = JSON.parse(loadFile(repoURL + "sileo-featured.json"))
 } catch (error) {
-    var repoTitle = "Repo Title"
+    validSileoFeatured = false
 }
 
 // Set Repo Title
@@ -17,22 +33,11 @@ document.getElementById("websiteTitle").innerText  = repoTitle
 document.getElementById("repositoryName").innerText  = repoTitle
 
 // Set Page Icon
-document.getElementById("websiteIcon").href  = repoURL.toLowerCase() + "/CydiaIcon.png"
-
-// Get Sileo Featured JSON
-try {
-    var sileoFeaturedJSON = JSON.parse(loadFile(repoURL.toLowerCase() + "/sileo-featured.json"))
-} catch (err) {
-    var sileoFeaturedJSON = ""
-}
+document.getElementById("websiteIcon").href  = repoURL + "CydiaIcon.png"
 
 // Render Packages File
-try {
-    // Check Packages File Exists
-    var validPackagesFile = true
-
-    // Following line will throw an exception if packages file does not exist
-    var packagesFile = loadFile(repoURL.toLowerCase() + "/Packages")
+if (validPackagesFile) {
+    var packages = decodePackagesFile(packagesFile)
 
     // Set total packages (All Categories)
     const numOfPackages = packagesFile.match(/Package:/g).length
@@ -58,89 +63,59 @@ try {
         // Append TableButtonView to TableList
         document.getElementById("tableList").appendChild(tableButtonView)
     }
-} catch (err) {
-    validPackagesFile = false
-    alert("Failed to load Packages!")
 }
 
-// Define Banner Image
-var bannerImage = document.createElement("div")
-with (bannerImage) {
-    className = "bannerImage"
-    style.borderRadius = sileoFeaturedJSON.itemCornerRadius + "px"
-    style.width = sileoFeaturedJSON.itemSize.replace(/\{|\,.*\}/g,"") + "px"
-    style.height = sileoFeaturedJSON.itemSize.replace(/\{.*\,|\}|\s/g,"") + "px"
-}
-
-// Generate Sileo Featured
-for (i=0; i<sileoFeaturedJSON.banners.length; i++) {
-    // Create Banner
-    var bannerImageClone = bannerImage.cloneNode(true)
-    bannerImageClone.style.backgroundImage = "url('" + sileoFeaturedJSON.banners[i].url + "')"
-    // Create Tweak Name Text
-    var bannerTweakName = document.createElement("div")
-    bannerTweakName.className = "bannerTweakName"
-    bannerTweakName.innerText = sileoFeaturedJSON.banners[i].title
-    if (sileoFeaturedJSON.banners[i].hideShadow) {
-        bannerTweakName.style.textShadow = "none"
+// Render Sileo Featured
+if (validSileoFeatured) {
+    // Define Banner Image
+    var bannerImage = document.createElement("div")
+    with (bannerImage) {
+        className = "bannerImage"
+        style.borderRadius = sileoFeaturedJSON.itemCornerRadius + "px"
+        style.width = sileoFeaturedJSON.itemSize.replace(/\{|\,.*\}/g,"") + "px"
+        style.height = sileoFeaturedJSON.itemSize.replace(/\{.*\,|\}|\s/g,"") + "px"
     }
-    // Append Tweak Name Text to BannerImage
-    bannerImageClone.appendChild(bannerTweakName)
-    // Get HREF URL from Packages File
-    var alreadyAppended = false
-    if (validPackagesFile) {
-        // Remove first half of Packages File upto first match of bundle ID 
-        var bannerURL = packagesFile.slice(packagesFile.match(sileoFeaturedJSON.banners[i].package).index)
-        // Check Package File contains any Sileo Depictions (Prevent exception on null)
-        if (bannerURL.match(/SileoDepiction:.*/) != null) {
-            // Check Specific Package contains Sileo Depiction
-            if (bannerURL.match(/SileoDepiction:.*/).index < bannerURL.match(/Package:/).index) {
-                // Extract line with next instance of "SileoDepiction:"
-                bannerURL = bannerURL.match(/SileoDepiction:.*/)[0]
-                // Remove "SileoDepiction:" from the start of the line leaving just the URL
-                bannerURL = bannerURL.replace(/SileoDepiction:\s/g,"")
-                // Create Link Element around BannerImage
-                var bannerImageLink = document.createElement("a")
-                bannerImageLink.href = "../?json=" + bannerURL + "&name=" + sileoFeaturedJSON.banners[i].title
-                bannerImageLink.appendChild(bannerImageClone)
-                // Append to Featured View
-                featuredBannersView.appendChild(bannerImageLink)
-                // Set alreadyAppended to prevent bannerImageClone from being appended later
-                alreadyAppended = true
+
+    // Define featuredBannersView
+    const featuredBannersView = document.getElementById("FeaturedBannersView")
+
+    // Generate Sileo Featured as HTML
+    for (i=0; i<sileoFeaturedJSON.banners.length; i++) {
+        // Create Banner
+        var bannerImageClone = bannerImage.cloneNode(true)
+        bannerImageClone.style.backgroundImage = "url('" + sileoFeaturedJSON.banners[i].url + "')"
+        // Create Tweak Name Text
+        var bannerTweakName = document.createElement("div")
+        bannerTweakName.className = "bannerTweakName"
+        bannerTweakName.innerText = sileoFeaturedJSON.banners[i].title
+        if (sileoFeaturedJSON.banners[i].hideShadow) {
+            bannerTweakName.style.textShadow = "none"
+        }
+        // Append Tweak Name Text to BannerImage
+        bannerImageClone.appendChild(bannerTweakName)
+        // Get HREF URL from Packages File
+        var alreadyAppended = false
+        if (validPackagesFile) {
+            for (packageNum=0; packageNum<packages.length; packageNum++) {
+                if (packages[packageNum].Package == sileoFeaturedJSON.banners[i].package) {
+                    // Create Link Element around BannerImage
+                    var bannerImageLink = document.createElement("a")
+                    // Set the link HREF
+                    bannerImageLink.href = 
+                        "../?json=" + packages[packageNum].SileoDepiction 
+                        + "&name=" + packages[packageNum].Name
+                        + "&dev=" + packages[packageNum].Author.replace(/\<.*\>/g,"") //Replace to remove emails in triangular brackets
+                    // Append to Featured View
+                    bannerImageLink.appendChild(bannerImageClone)
+                    featuredBannersView.appendChild(bannerImageLink)
+                    // Set alreadyAppended to prevent bannerImageClone from being appended later
+                    alreadyAppended = true
+                }
             }
         }
-    }
-    // Append to Featured View (if couldn't find link in packages file)
-    if (!alreadyAppended) {
-        featuredBannersView.appendChild(bannerImageClone)
-    }
-}
-
-
-// Function to return all different categories and amount of tweaks in them
-function returnCategoryCount(categoriesDump) {
-    // Define Variables
-    let categories = []
-    let count = []
-    let contains = false
-    // Loop through all categories dump (duplicates in one giant array)
-    for (i=0; i<categoriesDump.length; i++) {
-        var tempCategoryName = categoriesDump[i].replace(/Section:\s/g,"")
-        contains = false
-        // Loop through existing categories
-        for (j=0; j<categories.length; j++) {
-            // If already in existing category, increment the counter
-            if (categories[j] == tempCategoryName) {
-                contains = true
-                count[j]++
-            }
-        }
-        // If not in existing category, add it to it.
-        if (!contains) {
-            categories.push(tempCategoryName)
-            count.push(1)
+        // Append to Featured View (if couldn't find link in packages file)
+        if (!alreadyAppended) {
+            featuredBannersView.appendChild(bannerImageClone)
         }
     }
-    // Return into 2 dimensional array
-    return [categories, count]
 }
